76025d32c9bc3fd1006bfab16f97053152ed16f8
coolchem <varun8686@gmail.com>
2016-10-25 21:52:14 -0600

    "releasing new version"

--- scripts/build.js
+++ scripts/build.js
@@ -6,52 +6,63 @@ var cpy = require("cpy");
 var path = require("path");
 var fs = require("fs-extra");
 
-var isDeployBuild = false;
 
 if(argv._ && argv._.length > 0) //look release build
 {
     var subCommand = argv._[0];
     if(subCommand.toLowerCase() === "release")
     {
-
-        if(argv.deploy)
-            isDeployBuild = true;
-        
         build(true);
     }
+
 }
 else //do dev build
 {
     build();
 }
 
-function bundleFiles(cb){
+function createStandAlone(cb){
 
     var stealTools = require("steal-tools");
 
     json = JSON.parse(fs.readFileSync(path.resolve("./package.json"), 'utf8'));
     var version = json.version;
+    var moduleName = json.name;
 
-    try {
-        fs.lstatSync(path.resolve("./standalone/rama.js"));
-        fs.copySync(path.resolve("./standalone/rama.js"), path.resolve("./standalone") + "/rama"+version+".js");
-    }
-    catch (e) {
-        //continue
-    }
+    var standAloneFile = "./standalone/" + moduleName;
 
-    stealTools.export({
+    var pathToStandAloneFile = standAloneFile + ".js";
+    var pathToStandAloneMinifiedFile = standAloneFile + ".min.js";
+
+    var buildOptions = {
         system: {
             config:path.resolve("./")+"/package.json!npm"
         },
         outputs: {
             "+standalone": {
-                dest: path.resolve("./standalone") + "/rama.js"
+                dest: pathToStandAloneFile
             }
         }
-    }).then(function(){
+    };
 
-        cb();
+    stealTools.export(buildOptions).then(function(){
+
+        buildOptions.outputs = {
+            "+standalone": {
+                dest: pathToStandAloneMinifiedFile,
+                minify:true
+            }
+        };
+
+        //buiding minified version
+        stealTools.export(buildOptions).then(function(){
+
+            cb()
+
+        },function(err){
+            cb(err)
+        })
+
 
     },function(err){
         cb(err)
@@ -64,8 +75,8 @@ function build(isRelease){
 
     if(isRelease)
         cmd = cmd + " --declaration";
-/*    else
-        cmd = cmd + " --inlineSourceMap --inlineSources";*/
+    else
+        cmd = cmd + " --sourceMap";
 
     util.series(["npm run clean",cmd], function (err) {
 
@@ -79,23 +90,20 @@ function build(isRelease){
             if(isRelease)
             {
                 cpy(["**/*.js","**/*.d.ts"],"../dist",{cwd:process.cwd()+"/src",parents: true, nodir: true}).then(function(){
-                    
-                    if(!isDeployBuild)
-                    {
-                        bundleFiles(function (err) {
 
-                            if(err)
-                            {
-                                console.log(err);
-                                process.exit(1);
-                            }
-                            else
-                            {
-                                process.exit(0);
-                            }
-                        })
-                    }
-                    
+                    createStandAlone(function (err) {
+
+                        if(err)
+                        {
+                            console.log(err);
+                            process.exit(1);
+                        }
+                        else
+                        {
+                            process.exit(0);
+                        }
+                    })
+
                 },function(err){
 
                     console.log(err);
