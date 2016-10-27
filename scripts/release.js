04d6e5063837596de65eeb1ed8c3c28fafac6e9f
coolchem <varun8686@gmail.com>
2016-10-26 19:46:28 -0600

    preparing for publishing

--- scripts/release.js
+++ scripts/release.js
@@ -9,8 +9,6 @@ if(argv._ && argv._.length > 0) //look release build
 
     util.series(["npm test",
         "npm run build-release",
-        "git add -A",
-        "git commit -m" + '"'+"releasing new version" +'"',
         cmd,
         "git push","git push --tags"],function(err){
         if(err)
